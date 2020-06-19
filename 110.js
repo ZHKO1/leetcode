1dfcbf75e8c3087b7c1678d70811b62a2f7e5acd
ZHKO1 <zhko11993@gmail.com>
2020-06-19 18:04:41 +0800

    add 110

--- 110.js
+++ 110.js
@@ -1,3 +1,21 @@
 var isBalanced = function(root) {
+  return dfsIsBalanced(root) == -2 ? false: true;
 
-};
+  function dfsIsBalanced(node){
+    if(!node){
+      return 0;
+    }
+    if(!node.left && !node.right){
+      return 1;
+    }
+    var left = dfsIsBalanced(node.left);
+    if(left == -2)return -2;
+    var right = dfsIsBalanced(node.right);
+    if(right == -2)return -2;
+    if(Math.abs(left - right) <= 1){
+      return Math.max(left, right) + 1;
+    }else{
+      return -2;
+    }
+  }
+}
