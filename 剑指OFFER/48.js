d314056b9ecae60ac0f9ca37cbc14da813ba63a3
ZHKO1 <zhko11993@gmail.com>
2020-09-03 13:47:24 +0800

    add 剑指OFFER 48

---
+++ 剑指OFFER/48.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  if(s === ''){
+    return 0;
+  }
+  var Max = 0;
+  var map = {};
+  var left = 0;
+  var right = 0;
+  while(right <= s.length - 1){
+    var c = s[right];
+    if(!map[c]){
+      map[c] = 1;
+      Max = Math.max(Max, right - left + 1);
+      right++;
+    } else {
+      while(s[left] !== c && (left < right)){
+        var left_c = s[left];
+        map[left_c]--;
+        left++;
+      }
+      left++;
+      right++;
+    }
+  }
+  return Max;
+};
